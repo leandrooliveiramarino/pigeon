a9c65ae9bfa6789bdd3398d461f9cfc8e780c4b3
= <=>
2016-05-19 23:53:25 -0300

    Inserção de SlidingTabs e Fragments

--- app/src/main/java/pigeon/cursoandroid/com/pigeon/activity/MainActivity.java
+++ app/src/main/java/pigeon/cursoandroid/com/pigeon/activity/MainActivity.java
@@ -2,19 +2,32 @@ package pigeon.cursoandroid.com.pigeon.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.support.v7.widget.ToolbarWidgetWrapper;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
 import com.firebase.client.Firebase;
 
+import pigeon.cursoandroid.com.pigeon.Adapter.TabAdapter;
 import pigeon.cursoandroid.com.pigeon.R;
 import pigeon.cursoandroid.com.pigeon.application.ConfiguracaoFirebase;
+import pigeon.cursoandroid.com.pigeon.helper.SlidingTabLayout;
 
 public class MainActivity extends AppCompatActivity {
 
     private Firebase firebase;
-    private Button botaoSair;
+    private Toolbar toolbar;
+
+    private SlidingTabLayout slidingTabLayout;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,15 +36,48 @@ public class MainActivity extends AppCompatActivity {
 
         firebase = ConfiguracaoFirebase.getFirebase();
 
-        botaoSair = (Button) findViewById(R.id.btn_sair);
-        botaoSair.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                firebase.unauth();
-                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        });
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("Pigeon");
+        setSupportActionBar(toolbar);
+
+        slidingTabLayout = (SlidingTabLayout) findViewById(R.id.sld_tabs);
+        viewPager = (ViewPager) findViewById(R.id.vp_pagina);
+
+        //Configurar sliding tabs
+        slidingTabLayout.setDistributeEvenly(true);
+        slidingTabLayout.setSelectedIndicatorColors(ContextCompat.getColor(this, R.color.colorAccent));
+
+        //Configurar adapter
+        TabAdapter tabAdapter = new TabAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(tabAdapter);
+
+        slidingTabLayout.setViewPager(viewPager);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch(item.getItemId()) {
+            case R.id.item_sair:
+                deslogarUsuario();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void deslogarUsuario(){
+        firebase.unauth();
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
